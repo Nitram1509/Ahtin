6561b3cd411fbd36aedc1841129b3107e3d32e2d
layton736 <ahmed.salame@stud.hs-mannheim.de>
2015-06-22 00:04:40 +0200

    CHANGE: buchanlegen.php, Favortein DB eing. - Salame

--- 6.Meilenstein/php/buchanlegen.php
+++ 6.Meilenstein/php/buchanlegen.php
@@ -5,7 +5,6 @@
 	$dbname = "myBooks";
 	//$autor_id, $genre_id, $art_id;
 	
-	
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	// Check connection
@@ -16,12 +15,10 @@
 	// TABELLE Buch WIRD HIER BEARBEITET---------------
 	
 	$sql = "SELECT isbn FROM Buch WHERE isbn = ".$_GET["isbn"]." ; ";
-	 
-	$row = mysqli_fetch_assoc( mysqli_query($conn, $sql) ); 
-
+	$row_buch = mysqli_fetch_assoc( mysqli_query($conn, $sql) ); 
 	
 	//Buch anlegen oder buch vorhanden
-	if($row["isbn"] == ""){
+	if($row_buch["isbn"] == ""){
 		//buch anlegen
 		$autor_id = readAutor($conn, $_GET["autor"]);
 		
@@ -41,7 +38,6 @@
 				'".$_GET["auflage"]."', 
 				$genre_id );";
 		mysqli_query($conn, $sql);		
-		
 	}
 	
 	// TABELLE User WIRD HIER BEARBEITET---------------
@@ -50,33 +46,41 @@
 		WHERE vorname = '".$_GET["vorname"]."' 
 		AND	  nachname = '".$_GET["name"]."'";
 		 
-	$row = mysqli_fetch_assoc( mysqli_query($conn, $sql) ); 
+	$row_user = mysqli_fetch_assoc( mysqli_query($conn, $sql) ); 
 	
 	//User anlegen oder User vorhanden
-	if($row["id"] == ""){
+	if($row_user["id"] == ""){
 		//User anlegen
-		$sqluser = "INSERT INTO User (vorname,nachname)VALUES
+		$sql_user = "INSERT INTO User (vorname,nachname)VALUES
 				('".$_GET["vorname"]."' , '".$_GET["name"]."' )";
-		mysqli_query($conn, $sqluser);		
-		$row = mysqli_fetch_assoc( mysqli_query($conn, $sql) );		
+		mysqli_query($conn, $sql_user);		
+		$row_user = mysqli_fetch_assoc( mysqli_query($conn, $sql) );		
 	}
 
-	//Favoriten Behandlung-----------------------------
+	//Favoriten Eintrag in die Datenbank-----------------------------
+	
+	$sql_user = "SELECT id FROM User 
+					WHERE vorname = '".$_GET["vorname"]."' 
+					AND	  nachname = '".$_GET["name"]."'";
+	$row_user = mysqli_fetch_assoc(mysqli_query($conn, $sql_user));	
+	
+	$sql_buch = "SELECT isbn FROM Buch WHERE isbn = ".$_GET["isbn"]." ; ";
+	$row_buch = mysqli_fetch_assoc(mysqli_query($conn, $sql_buch));
 	
 	
-	
-	
+	$sql_favorit = " INSERT INTO favoriten (user_id, buch_isbn, favorit) VALUES
+					( ".$row_user["id"].", ".$row_buch["isbn"].",'".$_GET["filmfavorit"]."' );";
+	mysqli_query($conn, $sql_favorit);
 	
 	
 	mysqli_close($conn);
 		
+	echo 'Ihre Daten wurden übermittelt. Vielen Dank für Ihr vertrauen, Sie Penner.' ;	
 		
 	//liest einen Datensatz raus
 	function readAutor($conn, $name){
 		
 		$sql = "SELECT id FROM Autor WHERE name = '".$name."'; ";
-		
-		
 		$row = mysqli_fetch_assoc(mysqli_query($conn, $sql)); 
 		
 		if($row["id"] == ""){
@@ -95,6 +99,6 @@
 	
 
 
-    echo 'Daten wurden geschrieben ' ;
+   
 
 ?>
