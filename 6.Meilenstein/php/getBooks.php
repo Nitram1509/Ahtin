4fa41712c65e1bbde2975a6261268b1d9ae5b7ac
JonWal <jonathan.wadheuer@gmail.com>
2015-06-22 11:34:49 +0200

    CHANGED: book_entry.html, getBooks.php; required bei Favorit entfernt, getBooks überabeitet - Waldheuer

--- 6.Meilenstein/php/getBooks.php
+++ 6.Meilenstein/php/getBooks.php
@@ -1,9 +1,50 @@
 <?php
 
+	$servername = "localhost";
+	$username = "root";//Nicht vorhanden
+	$password = "";//Nicht vorhanden
+	$dbname = "myBooks";
+	//$autor_id, $genre_id, $art_id;
+	
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	// Check connection
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+
     if ($_GET["art"] == "horror") {
-        echo file_get_contents('../json/horror_books.json');
+        $art = "Horror";
+        $bezeichnung = "horrordata";
+
     } else if ($_GET["art"] == "roman") {
-        echo file_get_contents('../json/roman_books.json');
+        $art = "Roman";
+        $bezeichnung = "romandata";
     }
+
+    $sql = "SELECT * FROM Buch  ";
+    $result = mysqli_query($conn, $sql);
+
+
+    echo "{  
+    		\"$bezeichnung\": [";
+
+	if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+  		while($row = mysqli_fetch_assoc($result)) {
+	        echo 	"\"autor\": \"Stephen King\",
+					\"titel\": \"". $row["titel"]."\",
+					\"kapitel\": ". $row["kapitel"].",
+					\"buchart\": \"Taschenbuch\",
+					\"ISBN\": ". $row["isbn"].",
+					\"erscheinungsjahr\": ". $row["jahr"].",
+					\"auflage\": ". $row["auflage"]."
+					}";
+
+		}
+
+	}	
+
+	echo "] }";
 
 ?>
